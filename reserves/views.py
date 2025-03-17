3c981c28f4c2970a164a2c5d7363afff709228d1
emnshkw <menshikov_2004@list.ru>
2025-03-17 18:26:44 +0300

    add sec

--- reserves/views.py
+++ reserves/views.py
@@ -2,7 +2,21 @@ from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import ReserveModel
+import requests
+import vk_api
+from vk_api.longpoll import VkLongPoll
 
+
+def send_message(phone):
+    vk = vk_api.VkApi(token='vk1.a.7nejSscLgVBsOxkwY6IIAl25LjY569jXp-Mhz4o_eU9rKROq1w86-dEJd-epPm8JLlsDRpS01JLPhwLA7JEa6rXsz_Sfz2-DQnORGCMTPeJFl-27MkEZWZJCVXshnJD6JBz4aSdljCMS5E-_0zqz8ucGAZTRvWC-scyztovl4wUeG9DIrOgnfR1_I6rjLjRPgzPoLKoCZouMiTl6K86Yag')
+    longpoll = VkLongPoll(vk)
+    post = {
+        'user_id': 235396096,
+        'message': f"Новое бронирование от {phone}",
+        'random_id': 0
+    }
+
+    vk.method('messages.send', post)
 
 class ReserveAPIView(APIView):
     permission_classes = (AllowAny,)
